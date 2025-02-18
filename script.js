510f53b81196a18502ca1841dc71b8e31dbd6d51
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-18 15:52:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 function stringChop(str, size) {
   // your code here
-	let array=[]
-	let a=size;
-	let s=""
-for(let i=0; i<=a; i++){
-		if(i!==a){
-			s+=str[i]
-		}else{
-			array.push(s)
-			a=size+size
-			s=""
-			
-		}
-
+	let array =[];
+	for(let i=0;i<str.length; i+=size){
+		array.push(str.slice(i,i+size))
 	}
 	return array
 	
