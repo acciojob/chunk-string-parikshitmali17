da7d388ab03d4a0418063fbea9f10763dafac8d2
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-18 15:33:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,76 @@
 function stringChop(str, size) {
   // your code here
+	let array=[]
+	let a=size;
+	let s=""
+for(let i=0; i<=a; i++){
+		if(i!==a){
+			s+=str[i]
+		}else{
+			array.push(s)
+			a=2*size
+			s=""
+			
+		}
+
+	}
 }
 
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
